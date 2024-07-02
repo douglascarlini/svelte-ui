cf1dbd6a2b75751b4ad9aaa2acff8d753916c408
Douglas Carlini <douglascarlini@gmail.com>
2024-07-02 20:48:14 -0300

    icon color

--- src/components/Icon.svelte
+++ src/components/Icon.svelte
@@ -4,6 +4,7 @@
     export let name: string;
     export let size: number = 14;
     export let group: string = "";
+    export let color: string = config.theme.color.accent;
 </script>
 
-<i class="{group.length > 0 ? `fa-${group}` : 'fa'} fa-{name}" style="width:{size}px; color: {config.theme.color.accent}" />
+<i class="{group.length > 0 ? `fa-${group}` : 'fa'} fa-{name}" style="width:{size}px; color: {color}" />
